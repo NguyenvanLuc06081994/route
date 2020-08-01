863348361fedec3d116c3a0cab72e201c5c003cf
NguyenvanLuc06081994 <nvluc0608@gmail.com>
2020-08-02 04:46:37 +0700

    time-zone

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/{timezone?}', function ($timezone = null) {
+    if (!empty($timezone)){
+        $time = new DateTime(date('y-m-d H:i:s'), new DateTimeZone('UTC'));
+        $time->setTimezone(new DateTimeZone(str_replace('-','/',$timezone)));
+        echo "Mui gio ban chon : ". $timezone . 'hien tai la : ' .$time->format('d-m-y H:i:s');
+    }
+    return view('index');
+});
